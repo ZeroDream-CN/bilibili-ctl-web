94707ed20de1c5cbf8dba1b08bcd26e9114a69b6
Akkariin Meiko <akkariins@gmail.com>
2024-11-09 23:25:00 +0800

    Update bctl.user.js

--- bctl.user.js
+++ bctl.user.js
@@ -6,7 +6,8 @@
 // @author       Akkariin
 // @match        https://member.bilibili.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
-// @grant        none
+// @grant        GM_cookie
+// @connect      member.bilibili.com
 // ==/UserScript==
 
 (function() {
@@ -55,7 +56,7 @@
 
     function CheckInstall() {
         const server = GetData('bctl_server', false);
-        const token = GetData('bctl_token', false);
+        const token  = GetData('bctl_token', false);
         if (!server || !token) {
             ShowServerDialog((server) => {
                 ShowTokenDialog((token) => {
@@ -69,27 +70,43 @@
         return true;
     }
 
-    $(document).ready(function() {
+    async function GetCookie() {
+        return new Promise((resolve, reject) => {
+            GM_cookie.list({ url: "https://member.bilibili.com/" }, function(cookies, error) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(cookies);
+                }
+            });
+        });
+    }
+
+    async function Init() {
         if (!CheckInstall()) {
             console.log('未安装 Bilibili 评论管理工具');
             return;
         }
         const updateInterval = 1 * 60 * 60 * 1000;
-        const now = new Date().getTime();
-        const lastUpdate = GetLastUpdate();
-        const cookie = document.cookie;
+        const now            = new Date().getTime();
+        const lastUpdate     = GetLastUpdate();
+        const cookies        = await GetCookie();
+        let   cookie         = '';
+        for (let i = 0; i < cookies.length; i++) {
+            cookie += `${cookies[i].name}=${cookies[i].value}; `;
+        }
         const cachedCookie = GetData('bctl_cookie', '');
         if (now - lastUpdate > updateInterval || cookie !== cachedCookie) {
             var server = GetData('bctl_server', false);
-            var token = GetData('bctl_token', false);
+            var token  = GetData('bctl_token', false);
             if (server !== false && token !== false) {
                 var ajaxObj = $.ajax({
-                    url: `${server}?action=updateCookie`,
-                    type: 'POST',
+                    url  : `${server}?action=updateCookie`,
+                    type : 'POST',
                     async: true,
-                    data: {
+                    data : {
                         cookie: cookie,
-                        token: token
+                        token : token
                     },
                     success: function() {
                         var json = JSON.parse(ajaxObj.responseText);
@@ -110,5 +127,9 @@
         } else {
             console.log('距离上次更新 Cookie 不足 24 小时');
         }
+    }
+
+    $(document).ready(() => {
+        Init();
     });
 })();
